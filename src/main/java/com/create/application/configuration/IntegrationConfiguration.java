c1f256aeca5d8a3505150d10a3afab52960ef4aa
create1st <sebastian.gil@gmail.com>
2015-12-26 20:40:11 +0100

    Clean up config

--- src/main/java/com/create/application/configuration/IntegrationConfiguration.java
+++ src/main/java/com/create/application/configuration/IntegrationConfiguration.java
@@ -17,27 +17,17 @@
 
 package com.create.application.configuration;
 
-import org.apache.commons.logging.impl.SLF4JLog;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.integration.launch.JobLaunchingGateway;
 import org.springframework.batch.integration.launch.JobLaunchingMessageHandler;
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.logging.LogLevel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
-import org.springframework.integration.channel.NullChannel;
-import org.springframework.integration.channel.QueueChannel;
-import org.springframework.integration.channel.interceptor.WireTap;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.integration.config.xml.LoggingChannelAdapterParser;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.filters.AcceptOnceFileListFilter;
@@ -45,13 +35,8 @@ import org.springframework.integration.file.filters.CompositeFileListFilter;
 import org.springframework.integration.file.filters.SimplePatternFileListFilter;
 import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.scheduling.PollerMetadata;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.scheduling.support.PeriodicTrigger;
-import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.transaction.TransactionManager;
 import java.io.File;
 
 @Configuration
@@ -60,10 +45,6 @@ import java.io.File;
         "com.create.batch.integration"
 })
 public class IntegrationConfiguration {
-//    @Bean
-//    public PlatformTransactionManager transactionManager() {
-//        return new JpaTransactionManager();
-//    }
 
     @Bean(name = PollerMetadata.DEFAULT_POLLER)
     public PollerMetadata defaultPoller() {
@@ -89,9 +70,9 @@ public class IntegrationConfiguration {
     }
 
     @Bean
-    @InboundChannelAdapter(value = "inboundFileChannel", poller = @Poller(cron="${ticket.poller.cron}"))
-    public MessageSource<File> fileMessageSource(@Value("${ticket.poller.path}")final String path,
-                                                 @Value("${ticket.poller.fileMask}")final String fileMask) {
+    @InboundChannelAdapter(value = "inboundFileChannel", poller = @Poller(cron = "${ticket.poller.cron}"))
+    public MessageSource<File> fileMessageSource(@Value("${ticket.poller.path}") final String path,
+                                                 @Value("${ticket.poller.fileMask}") final String fileMask) {
         final FileReadingMessageSource source = new FileReadingMessageSource();
         final CompositeFileListFilter<File> compositeFileListFilter = new CompositeFileListFilter<>();
         final SimplePatternFileListFilter simplePatternFileListFilter = new SimplePatternFileListFilter(fileMask);
@@ -99,7 +80,7 @@ public class IntegrationConfiguration {
         compositeFileListFilter.addFilter(simplePatternFileListFilter);
         compositeFileListFilter.addFilter(acceptOnceFileListFilter);
         source.setFilter(compositeFileListFilter);
-         source.setDirectory(new File(path));
+        source.setDirectory(new File(path));
         return source;
     }
 
@@ -108,21 +89,4 @@ public class IntegrationConfiguration {
     public JobLaunchingMessageHandler jobLaunchingGateway(final JobLauncher jobLauncher) {
         return new JobLaunchingMessageHandler(jobLauncher);
     }
-
-//    @ServiceActivator(inputChannel = "outboundJobRequestChannel")
-//    @Bean
-//    public JobLaunchingGateway jobLaunchingGateway(final JobLauncher jobLauncher) {
-//        JobLaunchingGateway jobLaunchingGateway = new JobLaunchingGateway(jobLauncher);
-//        jobLaunchingGateway.setRequiresReply(false);
-//        return jobLaunchingGateway;
-//    }
-
-//@Bean
-//public IntegrationFlow fileReadingFlow(@Value("${fpml.messages.input}") File directory) {
-//    return IntegrationFlows
-//            .from(s -> s.file(directory).patternFilter("*.xml"),
-//                    e -> e.poller(Pollers.fixedDelay(20000)))
-//    .....................
-//    .get();
-//}
 }
